f487c12f334e44624116904e0ee72459be5b123c
omash735-sudo <omash735@gmail.com>
2026-04-29 14:45:42 +0200

    Update Navbar.tsx

--- spectrumcosmo/src/components/storefront/Navbar.tsx
+++ spectrumcosmo/src/components/storefront/Navbar.tsx
@@ -13,7 +13,7 @@ const links = [
   { href: '/', label: 'Home' },
   { href: '/products', label: 'Products' },
   { href: '/reviews', label: 'Reviews' },
-  { href: '/#contact', label: 'Contact' },
+  { href: '/contact', label: 'Contact' },
   { href: '/about', label: 'About Us' },
 ]
 
