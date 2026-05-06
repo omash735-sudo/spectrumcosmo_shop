8c8f200f8326a246cc5b58e8b4c3f1ea2dad87cc
omash735-sudo <omash735@gmail.com>
2026-05-06 18:24:29 +0200

    Update route.ts

--- spectrumcosmo/src/app/api/rating/route.ts
+++ spectrumcosmo/src/app/api/rating/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { prisma } from '@/lib/prisma'
+import { prisma } from '@/lib/db'
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession()
