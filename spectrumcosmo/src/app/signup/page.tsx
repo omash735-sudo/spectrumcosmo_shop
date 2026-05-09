79779fb4926791d0267f2177deb52c681e50da16
omash735-sudo <omash735@gmail.com>
2026-05-09 21:02:31 +0200

    Update page.tsx

--- spectrumcosmo/src/app/signup/page.tsx
+++ spectrumcosmo/src/app/signup/page.tsx
@@ -41,7 +41,7 @@ export default function SignupPage() {
     }
   }
 
-  const logoSrc = "https://res.cloudinary.com/dfsvnaslv/image/upload/v1777984813/1002913280-removebg-preview_cwcz7u.png"
+  const logoSrc = "https://res.cloudinary.com/dfsvnaslv/image/upload/v1777984813/1002913281-removebg-preview_jblapw.png"
 
   return (
     <>
