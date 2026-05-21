065cf0b5bdc2e3760c414ec6328398ccfc2e733f
omash735-sudo <omash735@gmail.com>
2026-05-21 20:39:05 +0200

    Add ContentBlockRenderer component for dynamic rendering

---
+++ spectrumcosmo/src/components/storefront/ContentBlockRenderer.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import ProductCarouselBlock from './blocks/ProductCarouselBlock';
+import StaticCardBlock from './blocks/StaticCardBlock';
+import ImageTextBlock from './blocks/ImageTextBlock';
+import AnnouncementBlock from './blocks/AnnouncementBlock';
+import TrendingRequestsBlock from './blocks/TrendingRequestsBlock';
+
+interface ContentBlock {
+  id: string;
+  type: string;
+  title: string;
+  description: string;
+  content: any;
+}
+
+export default function ContentBlockRenderer({ block }: { block: ContentBlock }) {
+  switch (block.type) {
+    case 'product_carousel':
+      return <ProductCarouselBlock block={block} />;
+    case 'static_card':
+      return <StaticCardBlock block={block} />;
+    case 'image_text':
+      return <ImageTextBlock block={block} />;
+    case 'announcement':
+      return <AnnouncementBlock block={block} />;
+    case 'trending_requests':
+      return <TrendingRequestsBlock block={block} />;
+    default:
+      return null;
+  }
+}
