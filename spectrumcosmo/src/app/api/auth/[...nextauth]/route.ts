5d3d679f0d2ce31380f0d1167a5b46610bb5f165
omash735-sudo <omash735@gmail.com>
2026-07-22 17:24:45 +0200

    Update route.ts

--- spectrumcosmo/src/app/api/auth/[...nextauth]/route.ts
+++ spectrumcosmo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
-import { handlers } from "@/auth"
+import { handlers } from "@/auth";
 
-export const { GET, POST } = handlers
+export const runtime = 'nodejs';  
+
+export const GET = handlers.GET;
+export const POST = handlers.POST;
