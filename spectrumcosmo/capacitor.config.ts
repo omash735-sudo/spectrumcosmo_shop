e25d11ce33729b2ddd3bb9e714c761914d282934
omash735-sudo <omash735@gmail.com>
2026-08-26 01:04:04 +0200

    Update capacitor.config.ts

--- spectrumcosmo/capacitor.config.ts
+++ spectrumcosmo/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.spectrumcosmo.app',
   appName: 'SpectrumCosmo',
-  webDir: '.next',
+  webDir: 'out',
   server: {
     androidScheme: 'https'
   },
