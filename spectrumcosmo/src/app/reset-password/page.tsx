d81e5a242c8489c9d5a46df0c1f686cdc2024bf6
omash735-sudo <omash735@gmail.com>
2026-08-26 18:46:44 +0200

    Update page.tsx

--- spectrumcosmo/src/app/reset-password/page.tsx
+++ spectrumcosmo/src/app/reset-password/page.tsx
@@ -1,12 +1,12 @@
-export const dynamic = 'force-dynamic';
+'use client';
 
-import { Suspense } from 'react'
-import ResetPasswordForm from './ResetPasswordForm'
+import { Suspense } from 'react';
+import ResetPasswordForm from './ResetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
       <ResetPasswordForm />
     </Suspense>
-  )
+  );
 }
